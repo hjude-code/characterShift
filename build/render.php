c0f2e2fedd600e7d8d5e783a879b8c944de8a05f
hjude-design <harrison@hjude.com>
2024-02-11 19:13:41 -0500

    initial commit

---
+++ build/render.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
+ */
+
+ if(isset($_GET['message'])){
+	$message = $_GET['message'];
+ }else{
+	$message = $attributes['textCopy'];
+ }
+
+$words = explode(' ', $message);
+$offestWord = 0;
+$offsetChar = 0;
+$offsetStep = $attributes['offsetStep'];
+
+
+?>
+<h1 <?php echo get_block_wrapper_attributes(); ?>>
+	<?php
+		foreach($words as $wordIndex=>$word){
+			$chars = str_split($word);
+			echo '<span class="wordBox">';
+			foreach($chars as $charIndex=>$char){
+				echo '<span class="charBox" style="animation-delay:'.$offestWord+$offsetChar.'s;">';
+				echo '<span>'.$char.'</span>';
+				echo '<span>'.$char.'</span>';
+				echo '<span>'.$char.'</span>';
+				echo '</span>';
+
+				$offsetChar+=$offsetStep;
+			}
+			echo '</span>';
+
+			$offestWord+=$offsetStep;
+		}
+	?>
+</h1>
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'ad40918a8e603b52a08e');
