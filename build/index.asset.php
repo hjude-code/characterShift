6755b780d3c27baedc8386e6b6a156ba71090449
Harrison Jude <harrison@hjude.com>
2024-07-01 11:58:08 -0400

    Added margin controls

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'd1a7315fc88779bcf529');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '75e306ccfbbcbab8f80e');
--- build/index.js
+++ build/index.js
@@ -193,7 +193,7 @@ module.exports = window["wp"]["i18n"];
   \************************/
 /***/ ((module) => {
 
-module.exports = /*#__PURE__*/JSON.parse('{"$schema":"https://schemas.wp.org/trunk/block.json","apiVersion":3,"name":"hjude/character-shift","version":"0.1.0","title":"Character shift","category":"text","example":{},"supports":{"html":false,"color":{"background":true,"text":true,"heading":true},"typography":{"fontSize":true},"align":true},"attributes":{"textCopy":{"type":"string","default":"Hey there you, this is a block"},"offsetStep":{"type":"number","default":0.1}},"textdomain":"character-shift","editorScript":"file:./index.js","editorStyle":"file:./index.css","style":"file:./style-index.css","render":"file:./render.php","viewScript":"file:./view.js"}');
+module.exports = /*#__PURE__*/JSON.parse('{"$schema":"https://schemas.wp.org/trunk/block.json","apiVersion":3,"name":"hjude/character-shift","version":"0.1.0","title":"Character shift","category":"text","example":{},"supports":{"html":false,"color":{"background":true,"text":true,"heading":true},"spacing":{"margin":true},"typography":{"fontSize":true},"align":true,"position":{"sticky":true}},"attributes":{"textCopy":{"type":"string","default":"Hey there you, this is a block"},"offsetStep":{"type":"number","default":0.1}},"textdomain":"character-shift","editorScript":"file:./index.js","editorStyle":"file:./index.css","style":"file:./style-index.css","render":"file:./render.php","viewScript":"file:./view.js"}');
 
 /***/ })
 
