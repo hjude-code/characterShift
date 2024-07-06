f116731dc00788ad25005c527ea9214c9dc38738
hjude-design <harrison@hjude.com>
2024-07-06 15:52:55 -0400

    added a meta type value to switch between custom meta values and default post values

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n'), 'version' => '3a83a9949b6553213915');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n'), 'version' => '027caf9fe8c6778265e5');
