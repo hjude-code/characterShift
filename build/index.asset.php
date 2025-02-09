e748bb5cbc728a86035d3622b2002c0dd529dea7
hjude-design <harrison@hjude.com>
2025-02-09 10:40:28 -0500

    minor formatting cleaning

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n'), 'version' => '753d198670dc65d08016');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n'), 'version' => 'f59e61a9014938a41233');
